27f053c59b970c9ba5b946f3393b979d6b317311
Guangyu Sun <guangyu@knights.ucf.edu>
2024-12-12 22:16:51 -0500

    add fancier box from supervision

--- app_langloc.py
+++ app_langloc.py
@@ -18,11 +18,9 @@ llm_bbox_image = gr.Image(label="llm_bbox_image")
 examples = [
     ["assets/demo.jpeg", "I'm thirsty"]
 ]
-gr.Interface(fn=gradio_interface, 
-             inputs=[frame_input, question_input], 
+gr.Interface(fn=gradio_interface,
+             inputs=[frame_input, question_input],
              outputs=[objs, all_bbox_image, llm_bbox_image],
              title="Visual Question Answering",
              description="Upload a frame and ask a question about the objects in the frame.",
              examples=examples).launch()
-
-
