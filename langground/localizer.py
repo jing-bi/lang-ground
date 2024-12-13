4767e5ffb0a4d6786a8ae3a34b37716315497bfd
jing <me@jing.vision>
2024-12-12 21:49:03 -0500

    add threhold support for loc

--- langground/localizer.py
+++ langground/localizer.py
@@ -7,23 +7,24 @@ from pathlib import Path
 
 
 class OWL:
-    def __init__(self, threshold=0.4):
+
+    def __init__(self):
         model_name = "google/owlv2-large-patch14-ensemble"
         self.processor = Owlv2Processor.from_pretrained(model_name)
         self.model = Owlv2ForObjectDetection.from_pretrained(model_name).to("cuda")
         self.model.eval()
         self.objects_f = Path(__file__).parent / "objects.txt"
         self.objects = [line.strip() for line in self.objects_f.open().readlines()]
-        self.threshold = threshold
         self.device = "cuda"
-    def localize(self, image):
+
+    def localize(self, image, threshold=0.5):
         image = Image.fromarray(image)
         final = defaultdict(list)
         with torch.inference_mode():
             inputs = self.processor(text=self.objects, images=[image], return_tensors="pt").to(self.device)
             outputs = self.model(**inputs)
             target_sizes = torch.Tensor([image.size[::-1]]).to(self.device)
-            result = self.processor.post_process_object_detection(outputs=outputs, target_sizes=target_sizes, threshold=self.threshold)[0]
+            result = self.processor.post_process_object_detection(outputs=outputs, target_sizes=target_sizes, threshold=threshold)[0]
 
             boxes, scores, labels = result["boxes"], result["scores"], result["labels"]
             for box, score, label in zip(boxes, scores, labels):
