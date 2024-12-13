ffdc99a1b4cf50b25a2085a02a85aac537902d38
jing <me@jing.vision>
2024-12-13 10:41:30 -0500

    change the layout

--- app_langloc.py
+++ app_langloc.py
@@ -21,23 +21,32 @@ with gr.Blocks() as demo:
     gr.HTML(title)
 
     with gr.Row():
-        with gr.Column(scale=1):
-            loc_model_input = gr.Dropdown(
-                choices=["yolo", "owl"],
-                value="yolo",
-                label="Localization Model",
-            )
-            llm_model_input = gr.Dropdown(
-                choices=["Qwen/Qwen2.5-7B-Instruct", "OpenGVLab/InternVL2_5-8B", 
-                         "OpenGVLab/InternVL2_5-4B", "OpenGVLab/InternVL2_5-2B",
-                         "OpenGVLab/InternVL2_5-1B"],
-                value="OpenGVLab/InternVL2_5-2B",
-                label="LLM Model",
-            )
+
         with gr.Column(scale=1):
             frame_input = gr.Image(type="pil", label="Upload Frame")
 
         with gr.Column(scale=1):
+            with gr.Row():
+                with gr.Column(scale=1):
+
+                    loc_model_input = gr.Dropdown(
+                        choices=["yolo", "owl"],
+                        value="yolo",
+                        label="Localization Model",
+                    )
+                with gr.Column(scale=2):
+
+                    llm_model_input = gr.Dropdown(
+                        choices=[
+                            "Qwen/Qwen2.5-7B-Instruct",
+                            "OpenGVLab/InternVL2_5-8B",
+                            "OpenGVLab/InternVL2_5-4B",
+                            "OpenGVLab/InternVL2_5-2B",
+                            "OpenGVLab/InternVL2_5-1B",
+                        ],
+                        value="Qwen/Qwen2.5-7B-Instruct",
+                        label="LLM Model",
+                    )
             threshold_input = gr.Slider(minimum=0, maximum=1, value=0.4, step=0.1, label="Threshold")
             question_input = gr.Textbox(lines=2, placeholder="Enter your question here", label="Question")
             objs = gr.Textbox(label="Answer")
@@ -56,7 +65,6 @@ with gr.Blocks() as demo:
         # 创建新模型
         model = create_model(loc_model, llm_model)
         return model.localize(frame, question, threshold=threshold)
-
 
     submit_btn.click(
         fn=update_model_and_localize,
