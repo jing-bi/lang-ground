9931b306f89a163662bf0f125150ab7cfa00817e
jing <me@jing.vision>
2024-12-18 09:12:46 -0500

    add quick example to test async

--- example.py
+++ example.py
@@ -1,6 +1,13 @@
 from langground import LangGround
 import cv2
+import time
 
 image = cv2.imread('./assets/demo.jpeg')
 lg = LangGround()
-lg.localize(image, "i'm thirsty")
+# lg.localize(image, "i'm thirsty")
+while True:
+    start = time.time()
+    res = lg.select("i am thirsty", ["bottle", "water", "cup", "laptop"], block=True)
+    print(time.time() - start)
+    time.sleep(0.5)
+    print(res)
